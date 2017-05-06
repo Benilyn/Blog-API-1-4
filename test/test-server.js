aa9a01b3ae40083eca66d2dfee9054b706413fe5
Benilyn <benilyn.gonzales@gmail.com>
2017-05-06 02:15:00 -0700

    create test for GET blog

--- test/test-server.js
+++ test/test-server.js
@@ -1,8 +1,8 @@
 /* jshint esversion: 6 */
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const should = chai.should();
+const chai      = require('chai');
+const chaiHttp  = require('chai-http');
+const should	= chai.should();
 
 const {app, runServer, closeServer} = require('../server');
 
@@ -10,11 +10,33 @@ chai.use(chaiHttp);
 
 describe('Blog', function() {
 	before(function() {
-		return runServer;
+		return runServer();
 	}); //before function
 
 	after(function() {
-		return closeServer;
+		return closeServer();
 	}); //after function
-		
+
+	it('should list BlogPosts on GET', function() {
+		return chai.request(app)
+		.get('/')
+		.then(function(res) {
+			res.should.have.status(200);
+			res.should.be.json;
+			res.body.should.be.a('array');
+			res.body.length.should.be.at.least(1);
+			const requiredFields = [
+				'title',
+				'content',
+				'author',
+				'publishDate'
+			]; //const requiredFields
+			res.body.forEach(function(blog) {
+				blog.should.be.a('object');
+				blog.should.contain.keys(requiredFields);
+			}); //.forEach function(blog)
+
+		}); //.then
+	}); //it(should list blog on GET)
+
 }); //describe(Blog)
