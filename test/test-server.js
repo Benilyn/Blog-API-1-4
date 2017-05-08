5e77b30ff97499c3cf9f8dbff97ef3e9c03e4ba2
Benilyn <benilyn.gonzales@gmail.com>
2017-05-08 00:49:10 -0700

    create test for POST blog

--- test/test-server.js
+++ test/test-server.js
@@ -28,8 +28,35 @@ describe('Blog', function() {
 			res.body.forEach(function(blog) {
 				blog.should.be.a('object');
 			}); //.forEach function(blog)
-
-		}); //.then
+		}); //.then function
 	}); //it(should list blog on GET)
+
+	it('should add BlogPosts on POST', function() {
+		const newBlogPost = {
+			title: 'Testing POST',
+			content: 'This is a test for new blog post',
+			author: 'Lyn'
+		}; //const newBlogPost
+		return chai.request(app)
+		.post('/')
+		.send(newBlogPost)
+		.then(function(res) {
+			res.should.have.status(201);
+			res.should.be.json;
+			res.body.should.be.a('object');
+			res.body.should.include.keys(
+				'title',
+				'content',
+				'author',
+				'id',
+				'publishDate'
+			); //res.body.should.include.keys
+			res.body.id.should.not.be.null;
+			res.body.should.deep.equal(Object.assign(newBlogPost, {
+				id: res.body.id,
+				publishDate: res.body.publishDate
+			}));
+		}); //.then function
+	}); //it(should add BlogPosts on POST)
 
 }); //describe(Blog)
